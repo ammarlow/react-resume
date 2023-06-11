7e6d3c9718511489ee852280faaecce58069def4
Ammar Low <ammarlow.work@gmail.com>
2023-06-11 15:23:49 +0800

    push all code into repo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,239 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>AMMAR LOW | SOFTWARE DEVELOPER</h1>
+        <h2>TERENGGANU, MALAYSIA</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <b>Email</b>: ammarlow.work@gmail.com
+              </td>
+              <td>
+                <b>Phone</b>: +6 010-901 7586
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <b>LinkedIn</b>:{" "}
+                <a href="https://linkedin.com/in/ammar-low" target="_blank">
+                  linkedin.com/in/ammar-low
+                </a>
+              </td>
+              <td>
+                <b>Github</b>:{" "}
+                <a href="https://github.com/ammarlow" target="_blank">
+                  github.com/ammarlow
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div>
+          <div className="black-bar">
+            <h3 className="Tajuk">PROFESSIONAL SUMMARY</h3>
+          </div>
+          <p className="konten">
+            Experienced software developer with expertise in Laravel, VueJs,
+            ReactJs, MySQL and MariaDB. Skilled in front-end and back-end
+            development, I deliver high-quality solutions on time and within
+            budget. Committed to ongoing professional development and staying
+            current with industry advancements. Eager to learn new skills in
+            related fields.
+          </p>
+        </div>
+        <div>
+          <div className="black-bar">
+            <h3 className="Tajuk">PROFESSIONAL EXPERIENCE</h3>
+          </div>
+          <p className="konten">
+            <b>Temasek E-Commerce Sdn Bhd</b>
+            <br></br>
+            &nbsp; Programmer | 22 May, 2022 - Present
+            <ul>
+              <li>
+                Successfully migrated a raw PHP project to a new, faster, and
+                more scalable Laravel Framework with Vue.js for the front-end.
+                The upgraded application delivers improved performance,
+                security, and user experience.
+              </li>
+              <li>
+                Implemented and integrated Sendinblue email marketing system for
+                newsletters, resulting in increased client's customer engagement
+                and conversion rates.
+              </li>
+              <li>
+                Collaborated with support team, developed automated testing
+                using PHP unit test, and implemented industry best practices to
+                deliver a high-quality software and support for the clients.
+              </li>
+            </ul>
+            <b>Iinfin SME Sdn Bhd</b>
+            <br></br>
+            &nbsp; Backend Developer Intern | 10 October, 2021 - 01 April, 2022
+            <ul>
+              <li>
+                Responsible for designing and implementing the server-side
+                architecture and ensuring the seamless integration of the
+                back-end with the front-end, using October CMS and Laravel PHP.
+              </li>
+              <li>
+                Collaborated with a team to design and develop SMEDaily, a
+                comprehensive CMS for SME-related news, similar to The Star, BBC
+                news and Al-Jazeera.
+              </li>
+              <li>
+                Successfully delivered a high-quality product on time and within
+                budget, meeting all client requirements.
+              </li>
+            </ul>
+            <b>Firesell Sdn Bhd</b>
+            <br></br>
+            &nbsp; Junior Software Developer | 24 May, 2019 - 01 September, 2019
+            <ul>
+              <li>
+                Designed and developed a payment gateway solution for the
+                company, utilizing my expertise in programming languages such as
+                PHP, and Shell Scripting.
+              </li>
+              <li>
+                The development process involved researching and analyzing
+                industry-standard payment gateways, gathering requirements,
+                designing the solution architecture, and developing a functional
+                prototype.
+              </li>
+              <li>
+                The experience allowed me to gain a deeper understanding of the
+                CRM and complexities involved in payment gateway development and
+                to develop valuable skills that have been successfully applied
+                to other projects.
+              </li>
+            </ul>
+            <b>Firesell Sdn Bhd</b>
+            <br></br>
+            &nbsp; Software Developer Intern | 01 January, 2019 - 23 May, 2019
+            <ul>
+              <li>
+                Developed Link Rotator, a Laravel Blade-based system that allows
+                for the creation of shortened, trackable links, and distribution
+                of traffic to one or multiple destinations.
+              </li>
+              <li>
+                Implemented link analytics, API integrations, and optimized
+                performance to provide a seamless user experience.
+              </li>
+              <li>
+                Successfully delivered the project, meeting all client
+                requirements and delivering a reliable and scalable system.
+                Client reported an immense increment on their monthly sales.
+              </li>
+            </ul>
+          </p>
+        </div>
+        <div>
+          <div className="black-bar">
+            <h3 className="Tajuk">PROJECTS</h3>
+          </div>
+          <p className="konten">
+            <b>Him Clinic</b>
+            <br></br>
+            <ul>
+              <li>
+                Created the Him Clinic website using Shopify, employing
+                cost-effective strategies by code my own theme to meet the
+                customer's needs and minimize resource consumption.
+              </li>
+              <li>
+                Collaborated with the client to gather requirements, design, and
+                develop a user-friendly website that is optimized for search
+                engine performance.
+              </li>
+              <li>
+                The result was a highly successful project that enabled the
+                client to generate over RM100,000 in profit.
+              </li>
+            </ul>
+            <b>Terengganu Lawnet</b>
+            <br></br>
+            <ul>
+              <li>
+                Developed Terengganu Lawnet, a digital repository of law
+                documents, independently for the Terengganu State Government,
+                resulting in a 70% reduction in document retrieval time.
+              </li>
+              <li>
+                Gathered requirements, designed the system architecture,
+                developed the user-friendly interface, and implemented search
+                functionality and secure access controls.
+              </li>
+              <li>
+                The project was successfully delivered, providing the public
+                with an efficient and reliable tool for accessing law documents.
+                My work contributed to the government's efforts to modernize its
+                services and make legal resources more accessible to the public.
+              </li>
+            </ul>
+          </p>
+        </div>
+        <div>
+          <div className="black-bar">
+            <h3 className="Tajuk">EDUCATION</h3>
+          </div>
+          <p className="konten">
+            <b>2019 - 2022 | Universiti Sultan Zainal Abidin (UniSZA)</b>
+            <br></br>
+            Bachelor of Computer Science - Internet Computing (CGPA: 3.54)
+            <br></br>
+            <br></br>
+            <b>2019 - 2022 | Universiti Sultan Zainal Abidin (UniSZA)</b>
+            <br></br>
+            Diploma in Information Technology (CGPA: 3.42)
+          </p>
+        </div>
+        <div>
+          <div className="black-bar">
+            <h3 className="Tajuk">SKILLS</h3>
+          </div>
+          <ul className="custom-list">
+            <li>PHP</li>
+            <li>MySQL</li>
+            <li>Laravel</li>
+            <li>MariaDB</li>
+            <li>HTML</li>
+            <li>Git</li>
+            <li>CSS</li>
+            <li>Docker</li>
+            <li>Javascript</li>
+            <li>VueJS</li>
+            <li>ReactJS</li>
+            <li>FTP</li>
+            <li>WordPress</li>
+            <li>Bootstrap CSS</li>
+            <li>Tailwind CSS</li>
+            <li>API</li>
+            <li>NodeJS</li>
+            <li>cPanel</li>
+          </ul>
+        </div>
+        <div>
+          <div className="black-bar">
+            <h3 className="Tajuk">REFERENCE</h3>
+          </div>
+          <ul className="reference-list">
+            <li>Muhammad Ammar bin Saiful</li>
+            <li>Co-Worker</li>
+            <li>+60 13-636 8473</li>
+            <li>Fadzilah Arifiin</li>
+            <li>University Tutor</li>
+            <li>+60 14-810 2725</li>
+          </ul>
+        </div>
+        <div className="footer">
+        </div>
       </header>
     </div>
   );
